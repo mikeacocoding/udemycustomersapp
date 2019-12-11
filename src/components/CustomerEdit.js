fe54a88f82db70149af327543d5f6448f7049611
Miguel Acosta <dev.miguel.acosta@gmail.com>
2019-12-10 20:55:01 -0500

    Implementación de Forms, creación de un HOC, y uso de los Initial Values

--- src/components/CustomerEdit.js
+++ src/components/CustomerEdit.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { reduxForm, Field } from 'redux-form';
+import { setPropsAsInitial } from '../helpers/setPropsAsInitial';
 
 const CustomerEdit = ({ name, dni, age }) => {
     return (
         <div>
             <h2>Edición del cliente</h2>
-            <h3>Nombre: {name} / Dni: {dni} / Edad: {age}</h3>
+
+            <form action="">
+                <div>
+                    <label htmlFor="name">Nombre:</label>
+                    <Field name="name" component="input" type="text" ></Field>
+                </div>
+                <div>
+                    <label htmlFor="dni">Dni:</label>
+                    <Field name="dni" component="input" type="text" ></Field>
+                </div>
+                <div>
+                    <label htmlFor="age">Age:</label>
+                    <Field name="age" component="input" type="number" ></Field>
+                </div>
+            </form>
         </div>
     );
 };
@@ -16,4 +33,6 @@ CustomerEdit.propTypes = {
     age: PropTypes.number,
 };
 
-export default CustomerEdit;
+const CustomerEditForm = reduxForm({ form: 'CustomerEdit' })(CustomerEdit);
+
+export default setPropsAsInitial(CustomerEditForm);
