54773f193f90345fbae3ae3c4a2d2538d47bab1b
Miguel Acosta <dev.miguel.acosta@gmail.com>
2019-12-10 19:53:16 -0500

    Sección 15 finalizada, hasta video 186, antes de Forms

--- src/containers/CustomerContainer.js
+++ src/containers/CustomerContainer.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import AppFrame from '../components/AppFrame';
+import { getCustomerByDni } from '../selectors/customers';
+import { Route } from 'react-router-dom';
+import CustomerEdit from '../components/CustomerEdit';
+import CustomerData from '../components/CustomerData';
 
 class CustomerContainer extends Component {
+
+    renderBody = () => (
+        <Route path="/customers/:dni/edit" children={
+            ({ match }) => {
+                const CustomerControl = match ? CustomerEdit : CustomerData;
+                return <CustomerControl {...this.props.customer} />
+            }
+        } />
+    )
+    //<p>Datos del cliente {this.props.customer.name}</p>
     render() {
         return (
             <div>
-                <AppFrame header='Cliente'
-                    body={<p>Datos del cliente</p>}>
+                <AppFrame header={`Cliente ${this.props.dni}`}
+                    body={this.renderBody()}>
                 </AppFrame>
             </div>
         );
@@ -16,7 +30,12 @@ class CustomerContainer extends Component {
 }
 
 CustomerContainer.propTypes = {
-
+    dni: PropTypes.string.isRequired,
+    customer: PropTypes.object.isRequired,
 };
 
-export default connect(null,null)(CustomerContainer);
+const mapStateToProps = (state, props) => ({
+    customer: getCustomerByDni(state, props)
+});
+
+export default connect(mapStateToProps, null)(CustomerContainer);
